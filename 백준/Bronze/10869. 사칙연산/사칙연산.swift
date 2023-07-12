a6e0dd0f5b2d071eed8e29ec1873183690680766
HAEUN KIM <hany01.dev@gmail.com>
2023-07-12 10:17:16 +0900

    [Bronze V] Title: 사칙연산, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.swift
+++ 백준/Bronze/10869. 사칙연산/사칙연산.swift
@@ -1,6 +1,2 @@
 var arr=readLine()!.split{$0==" "}.map{Int($0)!}
-print(arr[0]+arr[1])
-print(arr[0]-arr[1])
-print(arr[0]*arr[1])
-print(arr[0]/arr[1])
-print(arr[0]%arr[1])
+print("\(arr[0]+arr[1])\n\(arr[0]-arr[1])\n\(arr[0]*arr[1])\n\(arr[0]/arr[1])\n\(arr[0]%arr[1])")
