424f7076c0df9dedd227a410bf4b7b60808afc92
HAEUN KIM <hany01.dev@gmail.com>
2023-07-12 10:38:57 +0900

    [Bronze V] Title: 나머지, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.swift
+++ 백준/Bronze/10430. 나머지/나머지.swift
@@ -1,3 +1,3 @@
 let arr=readLine()!.split{$0==" "}.map{Int($0)!}
-let a=arr[0];let b=arr[1];let c=arr[2]
+let a=arr[0],b=arr[1],c=arr[2]
 print((a+b)%c,((a%c)+(b%c))%c,(a*b)%c,((a%c)*(b%c))%c,separator:"\n")
